c2696713c3d627b04d6a89e91bee9ca0c4b7014d
DearRabbit <DearRabbit@users.noreply.github.com>
2016-05-13 12:08:12 +0800

    sample without db

---
+++ database/settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'paykitty',
+    port: 3306
+}
--- routes/audit.js
+++ routes/audit.js
@@ -1,15 +1,61 @@
 var express = require('express');
 var path = require('path');
+
+//var conn = require('../database/connect');
 var router = express.Router();
 
 router.use(express.static(path.join(__dirname, '../public')));
 router.use(function(req, res, next) {
-  console.log('Hello');
+  if (!req.session.user && req.url != "/login") {
+    return res.redirect('./login');
+  }
   next();
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('audit_login', { title: 'Login'});
+  var loginFailFlag = false;
+  if (req.session.loginFailFlag == true) {
+    loginFailFlag = true;
+    req.session.loginFailFlag = false;
+  }
+  res.render('audit_login', { title: 'Login', login_fail: loginFailFlag});
+});
+
+router.post('/login', function(req, res, next) {
+  var testCase = {
+    username: 'admin',
+    password: 'admin',
+  };
+  if(req.body.username === testCase.username && req.body.password === testCase.password){
+    req.session.user = testCase.username;
+    return res.redirect('./home');
+  } else {
+    req.session.loginFailFlag = true;
+    req.session.user = null;
+    return res.redirect('./login');
+  }
+});
+
+router.get('/logout', function(req, res) {
+  req.session.user = null;
+  return res.redirect('./login');
+});
+
+router.get('/home', function(req, res, next) {
+  var data = [
+  {id: 0, buyer: 'buy01', seller: 'sell01', money: 10.02},
+  {id: 1, buyer: 'buy02', seller: 'sell01', money: 9999.99},
+  {id: 2, buyer: 'buy02', seller: 'sell02', money: 0.01},
+  ];
+  res.render('audit_home', { title: 'Home', data: data } );
+});
+
+router.get('/archive', function(req, res, next) {
+  res.render('audit_archive', { title: 'Archive'} );
+});
+
+router.get('/log', function(req, res, next) {
+  res.render('audit_log', { title: 'Log'} );
 });
 
 module.exports = router;
